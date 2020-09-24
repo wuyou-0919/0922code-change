d0f749b2d76803dd94c7b135b0b2ef12185d69f1
wuyou-0919 <2765024942@qq.com>
2020-09-24 23:31:33 +0800

    network request 1

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
@@ -12,4 +13,17 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "www.baidu.com"
 	c.Data["Email"] = "276502494@qq.com"
 	c.TplName = "index.tpl"
+}
+func (c *MainController) Post()  {
+	name := c.Ctx.Request.FormValue("name")
+	age := c.Ctx.Request.FormValue("age")
+	sex :=c.Ctx.Request.FormValue("sex")
+	fmt.Println(name,age,sex)
+	
+}
+
+func (c * MainController) post() {
+	for i := 0;i<10; i++ {
+		fmt.Printf("",i)
+	}
 }
